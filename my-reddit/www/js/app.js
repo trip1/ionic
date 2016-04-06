c784bee302c76810096d049a33c4f5b2315b180e
user <user>
2016-04-05 21:11:56 -0500

    added reddit

---
+++ my-reddit/www/js/app.js
@@ -0,0 +1,53 @@
+(function() {
+
+var app = angular.module('myreddit', ['ionic', 'angularMoment'])
+
+app.controller('RedditCtrl', function($http, $scope) {
+	
+	$scope.stories = [];
+	function loadStories(params, callback){
+		$http.get('http://www.reddit.com/r/all/hot/.json', {params: params})
+		.success(function(response){
+			var stories = [];
+			angular.forEach(response.data.children, function(child){
+					$scope.stories.push(child.data);
+			});
+			callback(stories);
+		});
+	}
+	
+	$scope.loadMore = function() {
+		var params = {};
+		if($scope.stories.length > 0){
+			params['after'] = $scope.stories[$scope.stories.length - 1].name;
+		}
+		
+		loadStories(params, function(olderStories){
+			$scope.stories = $scope.stories.concat(olderStories);
+			$scope.$broadcast('scroll.infiniteScrollComplete');
+		});
+	};
+	
+	$scope.loadNewerStories = function() {
+		var params = {'before': $scope.stories[0].name};
+		loadStories(params, function(newerStories){
+			$scope.stories = newerStories.concat($scope.stories);
+			$scope.$broadcast('scroll.refreshComplete');
+		})
+	};
+	
+});
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+	if(window.cordova && window.cordova.plugins.Keyboard) {
+	  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	  cordova.plugins.Keyboard.disableScroll(true);
+	}
+	if(window.StatusBar) {
+	  StatusBar.styleDefault();
+	}
+  });
+})
+
+}());
